726c777d58da4444ab2792a9b62d3a647ff04e4e
phantomguru0427 <dragonKnight0522@gmail.com>
2023-12-15 14:08:31 +0900

    product rate

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+const authRouter = require("./routes/auth");
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/product");
+
+app.use(cors());
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+
+const PORT = 3000;
+const DB =
+    "mongodb+srv://legend900619:mingtian2619@cluster0.htecqo0.mongodb.net/firstFlutter?retryWrites=true&w=majority";
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);
+});
